53b1b70e60e88332b0f8f49c74b586b9c08e2ae3
IevaVis <ieva.visniauskiene@gmail.com>
2018-10-08 21:15:36 +0200

    validations fix

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,4 @@
 class Chatroom < ApplicationRecord
 	belongs_to :user
+	validates :title, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   include Clearance::User
   enum role: [:teacher, :family, :student, :admin]
   validates :name, :country, presence: true
-  has_many :documents
-  has_many :diyprojects
-  has_many :chatrooms
+  has_many :documents, dependent: :destroy
+  has_many :diyprojects, dependent: :destroy
+  has_many :chatrooms, dependent: :destroy
 
 end
 
