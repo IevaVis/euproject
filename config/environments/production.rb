4d93fa33c5e797224e7d0b211c354262a6af63be
IevaVis <ieva.visniauskiene@gmail.com>
2018-10-08 13:41:04 +0200

    another config fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Images and documents saved in amazon.
-  config.active_storage.service = :amazon
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -42,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
