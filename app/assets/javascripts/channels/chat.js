45280a77d31c22db0ecdbd1e19ac75aea6c685fb
Pavel Kvasnikov <camokpyt@gmail.com>
2018-11-10 19:17:39 +0300

    Fix message notification link

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -6,15 +6,17 @@ App.chat = App.cable.subscriptions.create("ChatChannel", {
 
   },
   received: function(data) {
+    console.log(data);
   	var messages = $('#message-box');
     var message_template = data['message']['template'];
     var message_json = JSON.parse(data['message']['json_version']);
+
   	messages.append(message_template);
   	if (messages[0] !== undefined) {
       messages.scrollTop(messages[0].scrollHeight);
     }
     if (!window.location.href.endsWith('messages')) {
-      var template = "<a class='dropdown-item' href='#'>New message from " + message_json['from'] + "</a>"
+      var template = "<a class='dropdown-item' href='/en/conversations/" + message_json['conversation_id'] + "/messages'>New message from " + message_json['from'] + "</a>"
       var count = parseInt($("[data-behavior='unread-count']").text());
       count += 1;
       $("[data-behavior='unread-count']").text(count);
