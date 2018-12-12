9675d0bd644fb11ed315b415329b24acfbb878ff
IevaVis <ieva.visniauskiene@gmail.com>
2018-12-12 20:52:25 +0100

    clearance

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,10 +97,11 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'createskills.herokuapp.com' }
+  host = 'https://createskills.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
