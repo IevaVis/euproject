425affd5ca312b2f1cf8e2c22260a4611ae3c1e5
stiig <vasiliyrps2@gmail.com>
2018-12-19 23:45:32 +0500

    fix broadcasting

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,25 +1,36 @@
-App.messages = App.cable.subscriptions.create("MessagesChannel", {
-  connected: function() {
+// run after html rendering
+document.addEventListener('DOMContentLoaded', function () {
+    var message_box = $('#message-box');
+    // run only if we have one message box on the page
+    if (message_box.length === 1) {
+        App.messages = App.cable.subscriptions.create(
+            {
+                channel: 'MessagesChannel',
+                conversation_id: message_box.data('conversation-id'),
+            }, {
+                connected: function () {
 
-  },
-  disconnected: function() {
+                },
+                disconnected: function () {
 
-  },
-  received: function(data) {
-    var messages = $('#message-box');
-    var message_template = data['message']['template'];
-    var message_json = JSON.parse(data['message']['json_version']);
+                },
+                received: function (data) {
+                    var messages = $('#message-box')
+                    var message_template = data['message']['template']
+                    var message_json = JSON.parse(data['message']['json_version'])
 
-    messages.append(message_template);
-    if (messages[0] !== undefined) {
-      messages.scrollTop(messages[0].scrollHeight);
+                    messages.append(message_template)
+                    if (messages[0] !== undefined) {
+                        messages.scrollTop(messages[0].scrollHeight)
+                    }
+                    // if (!window.location.href.endsWith('messages')) {
+                    //   var template = "<a class='dropdown-item' href='/conversations/" + message_json['conversation_id'] + "/messages'>New message from " + message_json['from'] + "</a>"
+                    //   var count = parseInt($("[data-behavior='unread-count']").text());
+                    //   count += 1;
+                    //   $("[data-behavior='unread-count']").text(count);
+                    //   $("[data-behavior='notification-items']").append(template);
+                    // }
+                },
+            })
     }
-    // if (!window.location.href.endsWith('messages')) {
-    //   var template = "<a class='dropdown-item' href='/conversations/" + message_json['conversation_id'] + "/messages'>New message from " + message_json['from'] + "</a>"
-    //   var count = parseInt($("[data-behavior='unread-count']").text());
-    //   count += 1;
-    //   $("[data-behavior='unread-count']").text(count);
-    //   $("[data-behavior='notification-items']").append(template);
-    // }
-  }
-});
+})
--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,7 +2,7 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "messages", {
+    ActionCable.server.broadcast "messages_#{message.dig(:json_version, :conversation_id)}", {
     	message: render_message(message)
     }
   end
