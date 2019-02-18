79b8327cb4bb07aa48e21d02bdb948630f9158c3
IevaVis <ieva.visniauskiene@gmail.com>
2019-02-18 11:04:17 +0200

    spanish nav fix

--- app/controllers/passwords_controller.rb
+++
@@ -1,4 +0,0 @@
-class PasswordsController < Clearance::PasswordsController
-
-
-end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,17 +5,17 @@ class WelcomeController < ApplicationController
 	end
 
 	def terms
-		add_breadcrumb "Home", :root_path
+		add_breadcrumb t(".home"), :root_path
 		add_breadcrumb "Terms"
 	end
 
 	def about
-		add_breadcrumb "Home", :root_path
+		add_breadcrumb t(".home"), :root_path
 		add_breadcrumb "About Us"
 	end
 
 	def contacts
-		add_breadcrumb "Home", :root_path
+		add_breadcrumb t(".home"), :root_path
 		add_breadcrumb "Contacts"
 	end
 
